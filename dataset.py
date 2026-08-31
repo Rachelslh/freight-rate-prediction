fixed feature leakage

dataset.py
@@ -1,3 +1,4 @@
+import pickle
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import haversine_distances
@@ -9,9 +10,11 @@ class Dataset:
         self.val_rate = config.VAL_FRACTION
         self.cfg = config
 
-        self.build_features()
         self.split()
-        
+        self.frequency_features = None
+        self.train_df = self.build_features(self.train_df)
+        self.val_df = self.build_features(self.val_df)
+                
         self.x = self.train_df.drop(columns=["posted_rate"])
         self.y = self.train_df["posted_rate"]
         self.y_log = np.log1p(self.y)
@@ -20,9 +23,7 @@ class Dataset:
         self.y_val = self.val_df["posted_rate"]
         self.y_val_log = np.log1p(self.y_val)
         
-    def build_features(self) -> pd.DataFrame:
-        
-        df = pd.read_csv(self.dataset_path)
+    def build_features(self, df: pd.DataFrame) -> pd.DataFrame:
         
         df["date"] = pd.to_datetime(df["date"])
 
@@ -41,23 +42,47 @@ class Dataset:
 
         df["weight_missing"] = df["weight"].isna().astype(int)
         df["market_index_missing"] = df["market_index"].isna().astype(int)
-        df["weight"] = df["weight"].fillna(df["weight"].median())
-        df["market_index"] = df["market_index"].fillna(df["market_index"].median())
+        
+        self.weight_median = df["weight"].median()
+        df["weight"] = df["weight"].fillna(self.weight_median)
+        
+        self.market_index_median = df["market_index"].median()
+        df["market_index"] = df["market_index"].fillna(self.market_index_median)
 
         df["route"] = df["pickup"] + "__" + df["delivery"]
-        df["route_freq"] = df["route"].value_counts().fillna(0)
-        df["pickup_freq"] = df["pickup"].value_counts().fillna(0)
-        df["delivery_freq"] = df["delivery"].value_counts().fillna(0)
+        if self.frequency_features is None:
+            self.frequency_features = {
+            "route": df["route"].value_counts(),
+            "pickup": df["pickup"].value_counts(),
+            "delivery": df["delivery"].value_counts(),
+        }
+            
+        df["route_freq"] = df["route"].map(self.frequency_features["route"]).fillna(0)
+        df["pickup_freq"] = df["pickup"].map(self.frequency_features["pickup"]).fillna(0)
+        df["delivery_freq"] = df["delivery"].map(self.frequency_features["delivery"]).fillna(0)
 
-        self.df = pd.get_dummies(df, columns=["equipment"], prefix="eq")
+        df = pd.get_dummies(df, columns=["equipment"], prefix="eq")
 
+        new_df = df.drop(columns=["load_id", "pickup", "delivery", "route", "date"])
+        return new_df
 
     def split(self) -> pd.DataFrame:
-        df = self.df.sort_values("date").reset_index(drop=True)
+        df = pd.read_csv(self.dataset_path)
+        
+        df = df.sort_values("date").reset_index(drop=True)
         split_idx = int(len(df) * (1 - self.val_rate))
         cutoff_date = df.iloc[split_idx]["date"]
-        train_df = df[df["date"] < cutoff_date]
-        val_df = df[df["date"] >= cutoff_date]
+        self.train_df = df[df["date"] < cutoff_date]
+        self.val_df = df[df["date"] >= cutoff_date]
             
-        self.train_df = train_df.drop(columns=["load_id", "pickup", "delivery", "route", "date"])
-        self.val_df = val_df.drop(columns=["load_id", "pickup", "delivery", "route", "date"])
+    def save_artifacts(self, ):
+        data_to_save = {
+            "freq_maps": self.frequency_features,
+            "impute_values": {
+                "weight_median": self.weight_median,
+                "market_index_median": self.market_index_median,
+            }
+        }
+
+        with open("model_artifacts.pkl", "wb") as f:
+            pickle.dump(data_to_save, f)